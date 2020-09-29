b97548f19d9306add519c6d4d9bf80dc419e2f04
leekafai <leekafai95@outlook.com>
2020-09-29 10:59:11 +0800

    test

--- test/node/count.test.js
+++ test/node/count.test.js
@@ -5,17 +5,18 @@ const expect = require('chai').expect
 describe('计时', function () {
   this.timeout(6e4)
   describe('Start Stop', function () {
-    it('2e3 执行任务，返回大于2e3', function () {
+    it('2e3 执行任务，返回大于2e3', function (done) {
       const w = stopwatch().Start()
       w.Block(2e3)
       const ms = w.Stop()
       console.log(ms, '>>>>>>>')
       expect(ms).gte(2e3)
+      done()
     });
   })
 
   describe('Pause Continue', function () {
-    it('1e3 后暂停 3e3 , 再恢复，执行 1e3。有效计时 2e3', function () {
+    it('1e3 后暂停 3e3 , 再恢复，执行 1e3。有效计时 2e3', function (done) {
       const w = stopwatch().Start()
       w.Block(1e3)
       w.Pause()
@@ -25,10 +26,11 @@ describe('计时', function () {
       const ms = w.Stop()
       console.log(ms, '>>>>>>>')
       expect(ms).gte(2e3).lt(3e3)
+      done()
     });
   })
   describe('Split', function () {
-    it('1e3,2e3,3e3,总共 6e3', function () {
+    it('1e3,2e3,3e3,总共 6e3', function (done) {
       const w = stopwatch().Start()
       w.Block(1e3)
       expect(w.Split()).gte(1e3).lt(2e3)
@@ -39,10 +41,11 @@ describe('计时', function () {
       const ms = w.Stop()
       console.log(ms, '>>>>>>>')
       expect(ms).gte(6e3).lt(7e3)
+      done()
     });
   })
   describe('Restart', function () {
-    it('计时 2e3 后 重置计时 1e3', function () {
+    it('计时 2e3 后 重置计时 1e3', function (done) {
       const w = stopwatch().Start()
       w.Block(2e3)
       w.Stop()
@@ -51,13 +54,15 @@ describe('计时', function () {
       const ms = w.Stop()
       console.log(ms, '>>>>>>>')
       expect(ms).gte(1e3).lt(2e3)
+      done()
     });
   })
   describe('Restart On(Stop', function () {
-    it('计时 2e3 后 重置计时 1e3。使用 On Stop', function () {
+    it('计时 2e3 后 重置计时 1e3。使用 On Stop', function (done) {
       const w = stopwatch().Start().On(`Stop`, ({ ms }) => {
         console.log(ms, '首次 on')
         expect(ms).gte(2e3).lt(3e3)
+        done()
       })
       w.Block(2e3)
       w.Stop()
--- test/node/countdown.test.js
+++ test/node/countdown.test.js
@@ -3,7 +3,7 @@ const stopwatch = require('../../index')
 const expect = require('chai').expect
 describe(`倒计时`, function () {
   this.timeout(6e4)
-  it(`倒计 3e3 , on Countdown/timeout`, function () {
+  it(`倒计 3e3 , on Countdown/timeout`, function (done) {
     // return
 
     const sw = stopwatch()
@@ -12,10 +12,11 @@ describe(`倒计时`, function () {
       console.log(sw.label)
       console.log('Countdown/timeout-1', ms)
       expect(ms).gte(3e3).lte(4e3)
+      done()
     })
       .Countdown(3e3)
   })
-  it(`倒计时重启`, () => {
+  it(`倒计时重启`, (done) => {
     // return
     const sw = stopwatch()
     sw.On('Countdown/timeout', ({ ms }) => {
@@ -23,11 +24,12 @@ describe(`倒计时`, function () {
 
       console.log('Countdown/timeout-2', ms)
       expect(ms).gte(2e3).lte(3e3)
+      done()
     })
     sw.Countdown(4e3)
     sw.CountdownRestart(2e3)
   })
-  it(`设置 5e3 开始, 1e3 后剩余 4e3 暂停 2e3 再继续倒计时`, () => {
+  it(`设置 5e3 开始, 1e3 后剩余 4e3 暂停 2e3 再继续倒计时`, (done) => {
     // return
     // 5e3 - 1e3 剩余 4e3 时暂停，然后再倒计时
     // 总倒计时 5e3，实际时间花销 5e3 + 2e3 
@@ -37,6 +39,7 @@ describe(`倒计时`, function () {
       console.log(sw.label)
       console.log('Countdown/timeout-3', ms, real_ms)
       expect(ms).gte(timeout).lte(timeout + 1e3)
+      done()
     })
     sw.Countdown(timeout)
     sw.Block(1e3) // -1e3 
